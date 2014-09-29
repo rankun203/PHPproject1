8efdbdaee9acb04beee9757a30486dd5b4a004ef
rankun203 <rankun203@gmail.com>
2014-09-29 21:33:55 +0800

    init

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>PHP Test</title>
+        <script src="js/live.js" type="text/javascript"></script>
+    </head>
+    <body>
+        <?php
+        $name = "Sammy";
+        echo "Haha, $name.";
+        ?>
+    </body>
+</html>
